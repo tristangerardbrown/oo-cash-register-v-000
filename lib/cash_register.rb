964670a1f0a54c3378bdecf0d8c9073fbd3562c4
Tristan Brown <tgbrown@stanford.edu>
2018-09-18 22:54:02 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,26 @@
+class CashRegister
 
+  attr_accessor :total, :discount
+
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    items = []
+  end
+
+  def add_item (title, price, quantity = 1)
+    @total += price * quantity
+    @total 
+  end
+
+  def apply_discount
+    after_discount = @total * (.1 * (100 - @discount))
+  end
+
+  def items
+  end
+
+  def void_last_transaction
+  end
+
+end
